3ad3281be20970becad7d034b26902825d4d7b6b
AnishRane13 <anishrane178@gmail.com>
2025-07-03 19:37:35 +0530

    Solved Atoi

---
+++ Strings/Medium/String_To_Integer_(atoi).js
@@ -0,0 +1,40 @@
+var myAtoi = function (s) {
+  s = s.trim();
+
+  let isNeg = false;
+  let res = 0;
+  let val = 0;
+  let i = 0;
+
+    const INT_MIN = -(2**31);     
+  const INT_MAX = 2**31 - 1;
+
+  if (s[0] === '-') {
+    isNeg = true;
+    i++;
+  } else if (s[0] === '+') {
+    i++;
+  }
+
+
+  for (; i < s.length; i++) {
+    val = s.charCodeAt(i) - 48;
+
+    if (val >= 0 && val <= 9) {
+      res = res * 10 + val;
+    } else {
+      break;
+    }
+  }
+
+  if (isNeg) {
+    res = -res;
+  }
+
+   if (res < INT_MIN) return INT_MIN;
+  if (res > INT_MAX) return INT_MAX;
+
+  return res;
+};
+
+console.log(myAtoi("-91283472332"));
