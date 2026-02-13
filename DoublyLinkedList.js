1dcdeaef6344a1c6ec8f41e7e5172fd360c50ee6
AnishRane13 <113831709+AnishRane13@users.noreply.github.com>
2026-02-13 22:41:23 +0530

    Add insertAtEnd and delete methods to DoublyLinkedList

--- DoublyLinkedList.js
+++ DoublyLinkedList.js
@@ -25,6 +25,61 @@ class DoublyLinkedList{
         this.head = newNode;
         this.size++;
     }
+
+    insertAtEnd(data){
+        const newNode = new Node(data)
+
+        if (this.head === null) {
+            this.head = newNode;
+            this.size++;
+            return
+        }
+
+        let current = this.head;
+
+        while (current.next !== null) {
+            current = current.next;
+        }
+
+        current.next = newNode;
+        newNode.prev = current;
+        this.size++;
+    }
+
+    deleteFromBeginning(){
+        if (this.size === 0) {
+            return false;
+        }
+        if (this.head.next === null) {
+            this.head = null;
+            this.size--;
+            return;
+        }
+
+        this.head = this.head.next;
+        this.head.prev = null;
+        this.size--;
+    }
+
+    deleteFromEnd(){
+        if (this.head === null) {
+            return false;
+        }
+        if (this.head.next === null) {
+            this.head = null;
+            this.size--;
+            return;
+        }
+        
+        let current = this.head;
+        while (current.next.next !== null) {
+            current = current.next;
+        }
+
+        current.next = null;
+        this.size--;
+
+    }
 }
 
 const node1 = new Node(10);
@@ -32,5 +87,4 @@ const node1 = new Node(10);
 console.log(node1.data); // 10
 console.log(node1.next); // null
 console.log(node1.prev); // null
-
 
