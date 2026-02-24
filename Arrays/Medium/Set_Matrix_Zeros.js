d952fa9bd24b6e6f4ae0f7106dae513da9b75cbe
Anish <anish@mcplmumlptlen29.lan>
2026-02-24 08:29:11 +0530

    Started with Array medium problems

--- Arrays/Medium/Set_Matrix_Zeros.js
+++ Arrays/Medium/Set_Matrix_Zeros.js
@@ -5,25 +5,24 @@ var setZeroes = function(matrix) {
     const col = new Set();
 
     for (let i = 0; i < matrix.length; i++) {
-      
+    
         for (let j = 0; j < matrix[0].length; j++) {
-           if (matrix[i][j] == 0) {
-                row.add(i)
-                col.add(j)
-           }
+          if (matrix[i][j] === 0) {
+            row.add(i);
+            col.add(j);
+          }
         }
     }
 
-   for (let i = 0; i < matrix.length; i++) {
-      
+       for (let i = 0; i < matrix.length; i++) {
+    
         for (let j = 0; j < matrix[0].length; j++) {
-           if (row.has(i) || col.has(j)) {
-                matrix[i][j] = 0;
-           }
+          if (row.has(i) || col.has(j)) {
+            matrix[i][j] = 0;
+          }
         }
     }
-
-    return matrix;
+    return matrix
 };
 
 console.log(setZeroes([[1,1,1],[1,0,1],[1,1,1]]))
--- practice.js
+++ practice.js
@@ -1,125 +1,16 @@
-class Node{
-  constructor(data){
-      this.data = data;
-      this.next = null;
-  }
-}
+var twoSum = function(nums, target) {
+    const map = {};
 
-class LinkedList{
-  constructor(){
-    this.head = null
-    this.size = 0
-  }
+    for (let i = 0; i < nums.length; i++) {
+     let diff = target - nums[i];
 
-  insertAtBeginning(data){
-    const newNode = new Node(data);
-    newNode.next = this.head;
-    this.head = newNode;
-    this.size++;
-  }
+     if (map.hasOwnProperty(diff)) {
+      return [i, map[diff]];
+     }
 
-  insertAtEnd(data){
-    if (this.head == null) {
-      const newNode = new Node(data);
-      newNode.next = this.head;
-      this.head = newNode;
-      this.size++;
-      return
+     map[nums[i]] = i;
     }
+    return null;
+};
 
-    const newNode = new Node(data);
-    let current = this.head;
-
-    while (current.next !== null) {
-      current = current.next
-    }
-
-    current.next = newNode
-    this.size++;
-  }
-
-  deleteFromBeginning(){
-    if (this.head == null) {
-      return console.log("List is empty");
-    }else{
-      this.head = this.head.next;
-      this.size--;
-      return
-    }
-  }
-
-  deleteFromEnd(){
-    if (this.head == null) {
-      return console.log("list is empty");
-    }
-
-    if (!this.head.next) {
-      this.head = null;
-      this.size--;
-      return
-    }
-
-    let current = this.head
-    while (current.next.next) {
-      current = current.next
-    }
-
-    current.next = null;
-    this.size--;
-    return
-  }
-
-  search(data){
-    let current = this.head;
-
-    while (current !== null) {
-      if (current.data === data) {
-        return true;
-      }
-      current = current.next
-    }
-    return false;
-  }
-
-  reverse(){
-    let prev = null;
-    let current = this.head
-
-    while (current) {
-       const next = current.next; // store next
-  current.next = prev;       // reverse link
-  prev = current;            // move prev forward
-  current = next;            // move current forward
-    }
-    this.head = prev;
-  }
-
-  findMiddle(){
-    if (this.head === null) {
-        return null
-    }
-
-    let slow = this.head;
-    let fast = this.head;
-
-    while (fast !== null && fast.next !== null) {
-        slow = slow.next;
-        fast = fast.next.next;
-    }
-    return slow.data
-  }
-}
-
-const list = new LinkedList();
-
-list.insertAtEnd(10);
-list.insertAtEnd(20);
-list.insertAtEnd(30);
-list.search(20); // true
-list.search(100); // false
-
-
-console.log(list.head.data);               // 10
-console.log(list.head.next.data);          // 20
-console.log(list.head.next.next.data);     // 30
-console.log(list.size);                    // 3
+console.log(twoSum([2,7,11,15],9));
