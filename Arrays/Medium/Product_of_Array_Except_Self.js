0ea5ac7ae8fdc8cbe61454a884276ba399a8cb97
Anish <anish@mcplmumlptlen29.lan>
2026-02-27 10:11:37 +0530

    Done

--- Arrays/Medium/Product_of_Array_Except_Self.js
+++ Arrays/Medium/Product_of_Array_Except_Self.js
@@ -1,31 +1,36 @@
-// https://leetcode.com/problems/product-of-array-except-self/
+var productExceptSelf = function (nums) {
+    const n = nums.length;
+    const ans = new Array(n);
 
+    console.log("Input:", nums);
+    console.log("------ PREFIX PASS ------");
 
-var productExceptSelf = function(nums) {
-    const result = [];
-    let multipliedValue = 1;
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] === 0) {
-            continue;
-        }else{
-             multipliedValue *=  nums[i];
-        }
+    // Prefix pass
+    for (let i = 0, left = 1; i < n; ++i) {
+        console.log(`Before index ${i}: left = ${left}`);
+        ans[i] = left;
+        left *= nums[i];
+        console.log(`After index ${i}: ans = [${ans}], new left = ${left}`);
+        console.log("----");
     }
-    for (let i = 0; i < nums.length; i++) {
-        if (nums.includes(0)) {
-            if (nums[i] === 0) {
-                result.push(multipliedValue);
-            }else{
-                result.push(0)
-            }
-        }else{
-            result.push(multipliedValue/nums[i])
-        }
-    }
-    // console.log(nums.includes(0))
 
-    return result
+    console.log("After Prefix Pass:", ans);
+    console.log("------ SUFFIX PASS ------");
+
+    // Suffix pass
+    for (let i = n - 1, right = 1; i >= 0; --i) {
+        console.log(`Before index ${i}: right = ${right}`);
+        ans[i] *= right;
+        right *= nums[i];
+        
+        console.log(`After index ${i}: ans = [${ans}], new right = ${right}`);
+        console.log("----");
+    }
+
+    console.log("Final Answer:", ans);
+    console.log("=====================================");
+    return ans;
 };
 
-console.log(productExceptSelf([-1,1,0,-3,3]))
-console.log(productExceptSelf([0,0]))
+console.log(productExceptSelf([-1, 1, 0, -3, 3]));
+console.log(productExceptSelf([0, 0]));
