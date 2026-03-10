2d3b6583fefc60868cab57f26c29ff7a6cc0aaea
Anish <anish@mcplmumlptlen29.lan>
2026-03-10 08:29:19 +0530

    content children

--- Arrays/Easy/Best_Time_To_Buy_And_Sell_Stocks.js
+++ Arrays/Easy/Best_Time_To_Buy_And_Sell_Stocks.js
@@ -1,18 +1,31 @@
 // https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
 
+// var maxProfit = function (prices) {
+//     let prof = 0; mini = prices[0], maxi = 0;
+
+//     for (let i = 0; i < prices.length; i++) {
+//         if (prices[i] > mini) {
+//             prof = Math.max(prof, prices[i] - mini)
+//         }
+//         mini = Math.min(mini, prices[i])
+//     }
+//     return prof
+// };
+
 var maxProfit = function (prices) {
-    let prof = 0; mini = prices[0], maxi = 0;
+    
+    let profit = 0, mini = prices[0];
 
     for (let i = 0; i < prices.length; i++) {
         if (prices[i] > mini) {
-            prof = Math.max(prof, prices[i] - mini)
+            profit = Math.max(profit, prices[i]-mini)
         }
         mini = Math.min(mini, prices[i])
     }
-    return prof
+    return profit
 };
 
 console.log(maxProfit([7, 1, 5, 3, 6, 4]));
-// console.log(maxProfit([7,6,4,3,1]));
+console.log(maxProfit([7,6,4,3,1]));
 
 // need to check if diff is 0
