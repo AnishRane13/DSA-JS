9c9d8f8fbc5aafcd008095247ccfbf550214dc43
Anish <anish@mcplmumlptlen29.lan>
2026-03-12 00:15:52 +0530

    Reverse every word in a string

--- Strings/Medium/Reverse_Words_In_A_String.js
+++ Strings/Medium/Reverse_Words_In_A_String.js
@@ -1,40 +1,42 @@
-// var reverseWords = function(s) {
-//     let str = "";
-//     s = s.trim();
-//     const items = [];
-    
-
-//     for (let i = 0; i < s.length; i++) {
-//         if (s[i] === " ") {
-//             items.push(str);
-//             str = "";
-//         }else{
-//             str += s[i];
-//         }
-//     }
-//     items.push(str)
-//     str = "";
-
-//   while (items.length > 0) {
-//     let elem = items.pop();
-//     str += elem + " ";
-// }
-//     return str.trim()
-// };
-
-// console.log(reverseWords("a good   example"))
-
-var reverseWords = function(s) {
+var reverseWords = function (s) {
     let str = "";
+    s = s.trim();
     const items = [];
 
-    let result = s.split(/\s+/);
 
-    while (result.length > 0) {
-     str += result.pop() + " ";
-    //  console.log(str)
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === " ") {
+            if (str !== "") {
+                items.push(str);
+                str = "";
+            }
+        } else {
+            str += s[i];
+        }
+    }
+    items.push(str)
+    str = "";
+
+    while (items.length > 0) {
+        let elem = items.pop();
+        str += elem + " ";
     }
     return str.trim()
 };
+
+// console.log(reverseWords("a good   example"))
+
+// var reverseWords = function(s) {
+//     let str = "";
+//     const items = [];
+
+//     let result = s.split(/\s+/);
+
+//     while (result.length > 0) {
+//      str += result.pop() + " ";
+//     //  console.log(str)
+//     }
+//     return str.trim()
+// };
 
 console.log(reverseWords("a good   example"))
