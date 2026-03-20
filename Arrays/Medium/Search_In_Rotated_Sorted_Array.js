6e3a529e4c41a93c3da0c05c634bcdf7d3f84850
Anish Rane <anishrane178@gmail.com>
2026-03-20 07:40:30 +0530

    Search in a rotated array

--- Arrays/Medium/Search_In_Rotated_Sorted_Array.js
+++ Arrays/Medium/Search_In_Rotated_Sorted_Array.js
@@ -1,20 +1,32 @@
 // https://leetcode.com/problems/search-in-rotated-sorted-array/description/
 
-var search = function(nums, target) {
-    let low = nums[0], high = nums[nums.length-1]
+var search = function (nums, target) {
+    let low = 0, high = nums.length - 1;
+
 
     while (low <= high) {
-        let mid = ((low+high)/2)
+        let mid = Math.floor((low + high) / 2)
+
         if (nums[mid] === target) {
             return mid;
         }
 
         if (nums[low] <= nums[mid]) {
-            if (nums[low]) {
-                
+            if (nums[low] <= target && target <= nums[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        } else {
+            if (nums[mid] <= target && target <= nums[high]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
     }
+    return -1;
 };
 
-console.log(search([4,5,6,7,0,1,2],0))
+// console.log(search([4, 5, 6, 7, 0, 1, 2], 3))
+console.log(search([1,3], 1))
